af8051d504710608e379fa6d3e4fe8786461b80d
andogoiko <andoitz.goico@gmail.com>
2021-10-18 10:26:47 +0200

    el buen commmit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tutotodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
